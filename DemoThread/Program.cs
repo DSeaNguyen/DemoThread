f7363d1c26faa04201fe36bf2778649511209bf7
DSeaNguyen <nguyendinhhoangquan1825@gmail.com>
2024-11-04 09:36:33 +0700

    Add project files.

---
+++ DemoThread/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(() => {
+            DemoThread("Thread 1");
+        });
+        t.Start();
+
+        Thread t2 = new Thread(() => {
+            DemoThread("Thread 2");
+        });
+        t2.Start();
+
+        Thread t3 = new Thread(() => {
+            DemoThread("Thread 3");
+        });
+        t3.Start();
+
+        Console.ReadLine();
+    }
+
+    static void DemoThread(string threadIndex)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1)); //Giả lập độ trễ
+            Console.WriteLine(threadIndex + " - " + i);
+        }
+    }
+}
+
+
